2149013266320f5d2f654a866ac1154f6c262b67
Hyba <bouslahihiba55@gmail.com>
2025-11-25 18:16:57 +0100

    fixing jest errors

---
+++ frontend/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }]
+  ],
+};
+// this files helps us converts JSX syntax to regular JavaScript
